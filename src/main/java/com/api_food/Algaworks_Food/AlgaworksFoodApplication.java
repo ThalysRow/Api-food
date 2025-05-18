b13bc1457dd4d627112e2c7e26ea634779c73c3a
Thalys Row <thalysrogue@gmail.com>
2025-05-18 02:32:14 -0300

    first commit

---
+++ src/main/java/com/api_food/Algaworks_Food/AlgaworksFoodApplication.java
@@ -0,0 +1,13 @@
+package com.api_food.Algaworks_Food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgaworksFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgaworksFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_food/Algaworks_Food/AlgaworksFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_food.Algaworks_Food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgaworksFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
