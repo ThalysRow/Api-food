f4bb2dc5cb80680c515dc844295c1dcdba4aeca9
Thalys Row <thalysrogue@gmail.com>
2025-05-18 21:01:11 -0300

    feat(dto): add @NotBlank validation from starter-validation

--- src/main/java/com/api_food/Algaworks_Food/dto/KitchenDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/KitchenDTO.java
@@ -1,5 +1,6 @@
 package com.api_food.Algaworks_Food.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import java.util.UUID;
 @NoArgsConstructor
 public class KitchenDTO {
     private UUID id;
+    @NotBlank(message = "Kitchen name is required")
     private String name;
 }
