29a27d290636a394c31dae35e519e7f34db145c7
Thalys Row <thalysrogue@gmail.com>
2025-05-21 03:22:08 -0300

    feat(model): add StateModel Entity

---
+++ src/main/java/com/api_food/Algaworks_Food/model/StateModel.java
@@ -0,0 +1,25 @@
+package com.api_food.Algaworks_Food.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+    @OneToMany(mappedBy = "state")
+    private List<CityModel> cities;
+
+}
