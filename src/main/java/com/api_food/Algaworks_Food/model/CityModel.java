7d34045648f207ba0ef537dfa44ae12e2c99dac5
Thalys Row <thalysrogue@gmail.com>
2025-05-21 03:14:14 -0300

    feat(model): add CityModel Entity

---
+++ src/main/java/com/api_food/Algaworks_Food/model/CityModel.java
@@ -0,0 +1,23 @@
+package com.api_food.Algaworks_Food.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "state_id", nullable = false)
+    private StateModel state;
+}
