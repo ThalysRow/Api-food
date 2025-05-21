2eeda5581e87da78182b5b263c6f61dacfd04859
Thalys Row <thalysrogue@gmail.com>
2025-05-21 03:18:58 -0300

    refactor(model): include relationship with RestaurantModel

--- src/main/java/com/api_food/Algaworks_Food/model/KitchenModel.java
+++ src/main/java/com/api_food/Algaworks_Food/model/KitchenModel.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,4 +18,6 @@ public class KitchenModel {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String name;
+    @OneToMany(mappedBy = "kitchen")
+    private List<RestaurantModel> restaurants;
 }
