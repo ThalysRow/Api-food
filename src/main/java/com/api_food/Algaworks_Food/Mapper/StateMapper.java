6872dc2edf674de9698de47a3c2d8f419063e942
Thalys Row <thalysrogue@gmail.com>
2025-05-21 03:27:07 -0300

    feat(mapper): add StateMapper with conversion between StateDTO and StateModel

---
+++ src/main/java/com/api_food/Algaworks_Food/Mapper/StateMapper.java
@@ -0,0 +1,11 @@
+package com.api_food.Algaworks_Food.Mapper;
+
+import com.api_food.Algaworks_Food.dto.StateDTO;
+import com.api_food.Algaworks_Food.model.StateModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface StateMapper {
+    StateDTO toDTO(StateModel stateModel);
+    StateModel toEntity(StateDTO stateDTO);
+}
