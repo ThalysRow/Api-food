13ac613ed8b71b537b4cb86df4bd4d13aeed4891
Thalys Row <thalysrogue@gmail.com>
2025-05-23 07:10:59 -0300

    feat(Mapper): add RestaurantMapper

--- src/main/java/com/api_food/Algaworks_Food/Mapper/RestaurantMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/Mapper/RestaurantMapper.java
@@ -1,4 +1,11 @@
 package com.api_food.Algaworks_Food.Mapper;
 
+import com.api_food.Algaworks_Food.dto.RestaurantDTO;
+import com.api_food.Algaworks_Food.model.RestaurantModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface RestaurantMapper {
+    RestaurantDTO toDTO(RestaurantModel restaurantModel);
+    RestaurantModel toEntity(RestaurantDTO restaurantDTO);
 }
