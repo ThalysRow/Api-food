d267a5997332d6f4d5063f9e4eb75092ba594436
Thalys Row <thalysrogue@gmail.com>
2025-05-23 08:31:49 -0300

    fix(controller): inject restaurantService dependency

--- src/main/java/com/api_food/Algaworks_Food/controller/RestaurantController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/RestaurantController.java
@@ -4,19 +4,39 @@ import com.api_food.Algaworks_Food.dto.RestaurantDTO;
 import com.api_food.Algaworks_Food.service.RestaurantService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/restaurants")
 public class RestaurantController {
     private RestaurantService restaurantService;
 
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
     @PostMapping("/add")
     public ResponseEntity<RestaurantDTO> newRestaurant(@RequestBody RestaurantDTO restaurant){
         RestaurantDTO addRestaurant = restaurantService.newRestaurant(restaurant);
         return ResponseEntity.status(HttpStatus.CREATED).body(addRestaurant);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findRestaurant(@PathVariable UUID id){
+        try {
+            RestaurantDTO restaurant = restaurantService.findRestaurantById(id);
+            return ResponseEntity.ok(restaurant);
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<RestaurantDTO>> lisRestaurants(){
+        List<RestaurantDTO> restaurants = restaurantService.listRestaurants();
+        return ResponseEntity.ok(restaurants);
     }
 }
