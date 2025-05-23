e0b6bed193e482162ee258eaf5dbb9b5e4b008e1
Thalys Row <thalysrogue@gmail.com>
2025-05-23 07:10:21 -0300

    feat(repository): add RestaurantRepository

--- src/main/java/com/api_food/Algaworks_Food/repository/RestaurantRepository.java
+++ src/main/java/com/api_food/Algaworks_Food/repository/RestaurantRepository.java
@@ -1,4 +1,9 @@
 package com.api_food.Algaworks_Food.repository;
 
-public interface RestaurantRepository {
+import com.api_food.Algaworks_Food.model.RestaurantModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface RestaurantRepository extends JpaRepository<RestaurantModel, UUID> {
 }
