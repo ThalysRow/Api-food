7cb67ab7a8cb41da5c91ec8e0306037a79d05baf
Thalys Row <thalysrogue@gmail.com>
2025-05-23 07:11:42 -0300

    feat(service): add create new Restaurant

--- src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
@@ -1,4 +1,19 @@
 package com.api_food.Algaworks_Food.service;
 
+import com.api_food.Algaworks_Food.Mapper.RestaurantMapper;
+import com.api_food.Algaworks_Food.dto.RestaurantDTO;
+import com.api_food.Algaworks_Food.model.RestaurantModel;
+import com.api_food.Algaworks_Food.repository.RestaurantRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RestaurantService {
+    private RestaurantMapper restaurantMapper;
+    private RestaurantRepository restaurantRepository;
+
+    public RestaurantDTO newRestaurant(RestaurantDTO restaurant){
+        RestaurantModel newRestaurant = restaurantMapper.toEntity(restaurant);
+        RestaurantModel restaurantSave = restaurantRepository.save(newRestaurant);
+        return restaurantMapper.toDTO(restaurantSave);
+    }
 }
