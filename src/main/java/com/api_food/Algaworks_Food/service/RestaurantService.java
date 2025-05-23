2b547e3a6139f1a9b11f7703d52a8ff618b8937e
Thalys Row <thalysrogue@gmail.com>
2025-05-23 08:32:53 -0300

    fix(service): inject restaurant mapper and repository

--- src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
@@ -6,14 +6,31 @@ import com.api_food.Algaworks_Food.model.RestaurantModel;
 import com.api_food.Algaworks_Food.repository.RestaurantRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
 @Service
 public class RestaurantService {
     private RestaurantMapper restaurantMapper;
     private RestaurantRepository restaurantRepository;
 
+    public RestaurantService(RestaurantMapper restaurantMapper, RestaurantRepository restaurantRepository) {
+        this.restaurantMapper = restaurantMapper;
+        this.restaurantRepository = restaurantRepository;
+    }
+
     public RestaurantDTO newRestaurant(RestaurantDTO restaurant){
         RestaurantModel newRestaurant = restaurantMapper.toEntity(restaurant);
         RestaurantModel restaurantSave = restaurantRepository.save(newRestaurant);
         return restaurantMapper.toDTO(restaurantSave);
+    }
+
+    public RestaurantDTO findRestaurantById(UUID id){
+        RestaurantModel restaurant = restaurantRepository.findById(id).orElseThrow(()-> new RuntimeException("Restaurant not found"));
+        return restaurantMapper.toDTO(restaurant);
+    }
+
+    public List<RestaurantDTO> listRestaurants(){
+        return restaurantRepository.findAll().stream().map(restaurantMapper::toDTO).toList();
     }
 }
