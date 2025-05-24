6ea07cc5cc4d7ed3727af504bbb710e87855e1b2
Thalys Row <thalysrogue@gmail.com>
2025-05-24 15:18:54 -0300

    feat(repository): add CityRepository

--- src/main/java/com/api_food/Algaworks_Food/repository/CityRepository.java
+++ src/main/java/com/api_food/Algaworks_Food/repository/CityRepository.java
@@ -1,7 +1,6 @@
 package com.api_food.Algaworks_Food.repository;
 import com.api_food.Algaworks_Food.model.CityModel;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CityRepository extends JpaRepository<CityModel, Id> {
+public interface CityRepository extends JpaRepository<CityModel, Integer> {
 }
