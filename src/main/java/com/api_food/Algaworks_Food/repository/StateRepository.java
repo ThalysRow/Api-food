da93e0470c4edf0f1ac88b5215bcb0bb837c2850
Thalys Row <thalysrogue@gmail.com>
2025-05-24 15:22:50 -0300

    refactor(repository): include integer type in StateRepository

--- src/main/java/com/api_food/Algaworks_Food/repository/StateRepository.java
+++ src/main/java/com/api_food/Algaworks_Food/repository/StateRepository.java
@@ -1,8 +1,7 @@
 package com.api_food.Algaworks_Food.repository;
 
 import com.api_food.Algaworks_Food.model.StateModel;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StateRepository extends JpaRepository<StateModel, Id> {
+public interface StateRepository extends JpaRepository<StateModel, Integer> {
 }
