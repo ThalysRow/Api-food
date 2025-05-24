4ba679ab9caca29795f5634d11387f148bc6bd85
Thalys Row <thalysrogue@gmail.com>
2025-05-24 17:45:20 -0300

    feat(controller): add list all cities route

--- src/main/java/com/api_food/Algaworks_Food/controller/CityController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/CityController.java
@@ -4,10 +4,9 @@ import com.api_food.Algaworks_Food.dto.CityDTO;
 import com.api_food.Algaworks_Food.service.CityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
@@ -26,5 +25,11 @@ public class CityController {
         } catch (RuntimeException e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CityDTO>> listCities(){
+        List<CityDTO> cities = cityService.listCities();
+        return ResponseEntity.status(HttpStatus.OK).body(cities);
     }
 }
