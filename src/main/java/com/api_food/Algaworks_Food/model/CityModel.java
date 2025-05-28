2be12e5c53121e9ca449ca4d86d97395217f1986
Thalys Row <thalysrogue@gmail.com>
2025-05-28 02:35:48 -0300

    refactor(model): remove annotation not null

--- src/main/java/com/api_food/Algaworks_Food/model/CityModel.java
+++ src/main/java/com/api_food/Algaworks_Food/model/CityModel.java
@@ -1,8 +1,6 @@
 package com.api_food.Algaworks_Food.model;
 
-import com.api_food.Algaworks_Food.dto.StateDTO;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,7 +17,6 @@ public class CityModel {
     @Column(nullable = false)
     private String name;
     @ManyToOne
-    @NotNull
     @JoinColumn(name = "state_id", nullable = false)
     private StateModel state;
 }
