46ace9d8e984592070b599af61de1dd6f84f28d5
Thalys Row <thalysrogue@gmail.com>
2025-05-28 02:44:22 -0300

    feat(model): add payment methods entity

---
+++ src/main/java/com/api_food/Algaworks_Food/model/PaymentMethodModel.java
@@ -0,0 +1,23 @@
+package com.api_food.Algaworks_Food.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payment_methods")
+public class PaymentMethodModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "paymentMethod")
+    @JoinColumn(nullable = false)
+    private OrderModel order;
+}
