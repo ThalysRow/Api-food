096bea62c38a7b171a8d734a28e9d026594e6295
Thalys Row <thalysrogue@gmail.com>
2025-05-28 02:44:55 -0300

    feat(model): add permissions model entity

---
+++ src/main/java/com/api_food/Algaworks_Food/model/PermissionModel.java
@@ -0,0 +1,28 @@
+package com.api_food.Algaworks_Food.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "permissions")
+public class PermissionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @ManyToMany(mappedBy = "permissions")
+    private List<GroupModel> groups;
+}
