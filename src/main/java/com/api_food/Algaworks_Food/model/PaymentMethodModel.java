641eb6ef48fe9015f75ad49abff14b2060dd930d
Thalys Row <thalysrogue@gmail.com>
2025-05-29 02:43:25 -0300

    fix(model): Fixed relationship with OrderModel Entity

--- src/main/java/com/api_food/Algaworks_Food/model/PaymentMethodModel.java
+++ src/main/java/com/api_food/Algaworks_Food/model/PaymentMethodModel.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -18,6 +20,5 @@ public class PaymentMethodModel {
     private String name;
 
     @OneToMany(mappedBy = "paymentMethod")
-    @JoinColumn(nullable = false)
-    private OrderModel order;
+    private List<OrderModel> orders;
 }
