71bfe75027d8d97a24077203cb99fcae36525954
Thalys Row <thalysrogue@gmail.com>
2025-05-30 03:09:26 -0300

    refactor(dto): add restaurants in KitchenDTO

--- src/main/java/com/api_food/Algaworks_Food/dto/KitchenDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/KitchenDTO.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -14,4 +15,5 @@ public class KitchenDTO {
     private UUID id;
     @NotBlank(message = "Kitchen name is required")
     private String name;
+    private List<RestaurantDTO> restaurants;
 }
