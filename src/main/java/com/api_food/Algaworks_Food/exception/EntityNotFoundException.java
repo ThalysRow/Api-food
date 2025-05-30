fa2229d868fa31b7bc503ae98a148ee0da15df6e
Thalys Row <thalysrogue@gmail.com>
2025-05-30 02:59:16 -0300

    feat(exception): add EntityNotFoundException

---
+++ src/main/java/com/api_food/Algaworks_Food/exception/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package com.api_food.Algaworks_Food.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
