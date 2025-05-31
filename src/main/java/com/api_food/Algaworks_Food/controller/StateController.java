08fb56d6d4e427b9c6cd8468dfe84d397a80fa06
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:19:31 -0300

    refactor(controller): adjust all routes in StateController

--- src/main/java/com/api_food/Algaworks_Food/controller/StateController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/StateController.java
@@ -1,6 +1,8 @@
 package com.api_food.Algaworks_Food.controller;
 
-import com.api_food.Algaworks_Food.dto.StateDTO;
+import com.api_food.Algaworks_Food.dto.create.StateCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.StateListDTO;
+import com.api_food.Algaworks_Food.dto.update.StateUpdateDTO;
 import com.api_food.Algaworks_Food.service.StateService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -12,53 +14,39 @@ import java.util.List;
 @RestController
 @RequestMapping("/states")
 public class StateController {
-    private StateService stateService;
+    private final StateService stateService;
 
     public StateController(StateService stateService) {
         this.stateService = stateService;
     }
 
     @PostMapping("/new")
-    public ResponseEntity<StateDTO> newState(@Valid @RequestBody StateDTO state){
-        StateDTO newState = stateService.createNewState(state);
+    public ResponseEntity<StateCreateDTO> newState(@Valid @RequestBody StateCreateDTO state) {
+        StateCreateDTO newState = stateService.createNewState(state);
         return ResponseEntity.status(HttpStatus.CREATED).body(newState);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> findStateById(@PathVariable int id){
-        try {
-            StateDTO state = stateService.findStateById(id);
-            return ResponseEntity.status(HttpStatus.OK).body(state);
-        } catch (RuntimeException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    public ResponseEntity<StateListDTO> findStateById(@PathVariable int id) {
+        StateListDTO stateFinded = stateService.findStateById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(stateFinded);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<StateDTO>> listStates(){
-        List<StateDTO> states = stateService.listStates();
+    public ResponseEntity<List<StateListDTO>> listStates() {
+        List<StateListDTO> states = stateService.listStates();
         return ResponseEntity.status(HttpStatus.OK).body(states);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Object> updateState(@PathVariable int id, @RequestBody StateDTO state){
-        try {
-            StateDTO updateState = stateService.updateState(id, state);
-            return ResponseEntity.status(HttpStatus.OK).body(updateState);
-        } catch (RuntimeException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    public ResponseEntity<StateUpdateDTO> updateState(@PathVariable int id, @Valid @RequestBody StateUpdateDTO state) {
+        StateUpdateDTO stateUpdate = stateService.updateState(id, state);
+        return ResponseEntity.status(HttpStatus.OK).body(stateUpdate);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteState(@PathVariable int id){
-        try {
-             stateService.delState(id);
-             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        } catch (IllegalStateException e){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
-        } catch (RuntimeException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteState(@PathVariable int id) {
+        stateService.delState(id);
     }
 }
