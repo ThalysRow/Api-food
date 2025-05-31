72127be5ad0150efe11beaade0e000569f9d3b99
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:11:31 -0300

    refactor(mapper): add and adjust methods in CityMapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/CityMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/CityMapper.java
@@ -1,11 +1,16 @@
 package com.api_food.Algaworks_Food.mapper;
 
-import com.api_food.Algaworks_Food.dto.CityDTO;
+import com.api_food.Algaworks_Food.dto.create.CityCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.CityListDTO;
+import com.api_food.Algaworks_Food.dto.update.CityUpdateDTO;
 import com.api_food.Algaworks_Food.model.CityModel;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CityMapper {
-    CityDTO toDTO(CityModel cityModel);
-    CityModel toEntity(CityDTO cityDTO);
+    CityCreateDTO toCreateDTO(CityModel cityModel);
+    CityModel toCreateModel(CityCreateDTO cityCreateDTO);
+    CityListDTO toCreateListDTO(CityModel cityModel);
+    CityModel toUpdateModel(CityListDTO cityListDTO);
+    CityUpdateDTO toUpdateDTO(CityModel cityModel);
 }
