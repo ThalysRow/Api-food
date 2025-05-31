9985b0b834a5696f700db23d01e093829b0d4402
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:16:43 -0300

    refactor(service): adjust all methods in RestaurantService

--- src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/RestaurantService.java
@@ -1,13 +1,19 @@
 package com.api_food.Algaworks_Food.service;
 
+import com.api_food.Algaworks_Food.dto.create.RestaurantCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.KitchenListDTO;
+import com.api_food.Algaworks_Food.dto.list.RestaurantListDTO;
+import com.api_food.Algaworks_Food.dto.update.RestaurantUpdateDTO;
+import com.api_food.Algaworks_Food.exception.EntityNotFoundException;
+import com.api_food.Algaworks_Food.mapper.KitchenMapper;
 import com.api_food.Algaworks_Food.mapper.RestaurantMapper;
-import com.api_food.Algaworks_Food.dto.RestaurantDTO;
 import com.api_food.Algaworks_Food.model.KitchenModel;
 import com.api_food.Algaworks_Food.model.RestaurantModel;
-import com.api_food.Algaworks_Food.repository.KitchenRepository;
 import com.api_food.Algaworks_Food.repository.RestaurantRepository;
+import com.api_food.Algaworks_Food.utils.StringFormatter;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,45 +21,68 @@ import java.util.UUID;
 public class RestaurantService {
     private final RestaurantMapper restaurantMapper;
     private final RestaurantRepository restaurantRepository;
-    private final KitchenRepository kitchenRepository;
+    private final KitchenService kitchenService;
+    private final StringFormatter stringFormatter;
+    private final KitchenMapper kitchenMapper;
 
-    public RestaurantService(RestaurantMapper restaurantMapper, RestaurantRepository restaurantRepository, KitchenRepository kitchenRepository) {
+    public RestaurantService(RestaurantMapper restaurantMapper, RestaurantRepository restaurantRepository, KitchenService kitchenService, StringFormatter stringFormatter, KitchenMapper kitchenMapper) {
         this.restaurantMapper = restaurantMapper;
         this.restaurantRepository = restaurantRepository;
-        this.kitchenRepository = kitchenRepository;
+        this.kitchenService = kitchenService;
+        this.stringFormatter = stringFormatter;
+        this.kitchenMapper = kitchenMapper;
     }
 
-    public RestaurantDTO newRestaurant(RestaurantDTO restaurant){
-        UUID kitchenId = restaurant.getKitchen().getId();
-        KitchenModel kitchen = kitchenRepository.findById(kitchenId).orElseThrow(()-> new RuntimeException("Kitchen not found"));
+    public RestaurantCreateDTO newRestaurant(RestaurantCreateDTO restaurant){
 
-        RestaurantModel newRestaurant = restaurantMapper.toEntity(restaurant);
-        RestaurantModel restaurantSave = restaurantRepository.save(newRestaurant);
-        return restaurantMapper.toDTO(restaurantSave);
+        KitchenListDTO findKitchen = kitchenService.findKitchenById(restaurant.getKitchen().getId());
+        KitchenModel saveKitchen = kitchenMapper.toModel(findKitchen);
+
+        String formatedName = stringFormatter.stringFormated(restaurant.getName());
+
+        RestaurantModel newRestaurant = restaurantMapper.toCreateModel(restaurant);
+        newRestaurant.setName(formatedName);
+        newRestaurant.setDeliveryFee(restaurant.getDeliveryFee());
+        newRestaurant.setKitchen(saveKitchen);
+        newRestaurant.setDateCreated(LocalDateTime.now());
+        newRestaurant.setDateUpdated(LocalDateTime.now());
+
+        RestaurantModel saveRestaurant = restaurantRepository.save(newRestaurant);
+        return restaurantMapper.toCreateDTO(saveRestaurant);
+
     }
 
-    public RestaurantDTO findRestaurantById(UUID id){
-        RestaurantModel restaurant = restaurantRepository.findById(id).orElseThrow(()-> new RuntimeException("Restaurant not found"));
-        return restaurantMapper.toDTO(restaurant);
+    public RestaurantListDTO findRestaurantById(UUID id){
+        RestaurantModel restaurant = restaurantRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Restaurant not found"));
+        return restaurantMapper.toListDTO(restaurant);
     }
 
-    public List<RestaurantDTO> listRestaurants(){
-        return restaurantRepository.findAll().stream().map(restaurantMapper::toDTO).toList();
+    public List<RestaurantListDTO> listRestaurants(){
+        return restaurantRepository.findAll().stream().map(restaurantMapper::toListDTO).toList();
     }
 
-    public RestaurantDTO updateRestaurant(UUID id,RestaurantDTO restaurant){
+    public RestaurantUpdateDTO updateRestaurant(UUID id, RestaurantUpdateDTO restaurant){
 
-       RestaurantModel restaurantFinded = restaurantRepository.findById(id).orElseThrow(()-> new RuntimeException("Restaurant not found"));
-       UUID kitchenId = restaurant.getKitchen().getId();
-       KitchenModel kitchenFinded = kitchenRepository.findById(kitchenId).orElseThrow(()-> new RuntimeException("Kitchen not found"));
+        RestaurantListDTO findRestaurant = this.findRestaurantById(id);
+        KitchenListDTO findKitchen = kitchenService.findKitchenById(restaurant.getKitchen().getId());
 
-       RestaurantModel restaurantModel = restaurantMapper.toEntity(restaurant);
-        restaurantModel.setId(id);
-        restaurantModel.setKitchen(restaurantModel.getKitchen());
+        RestaurantModel updateRestaurant = restaurantMapper.toUpdateModel(findRestaurant);
+        KitchenModel updateKitchen = kitchenMapper.toModel(findKitchen);
+        String nameFormated = stringFormatter.stringFormated(restaurant.getName());
 
-        RestaurantModel restaurantUpdate = restaurantRepository.save(restaurantModel);
-        return restaurantMapper.toDTO(restaurantUpdate);
+        updateRestaurant.setName(nameFormated);
+        updateRestaurant.setDeliveryFee(restaurant.getDeliveryFee());
+        updateRestaurant.setKitchen(updateKitchen);
+        updateRestaurant.setDateUpdated(LocalDateTime.now());
 
+        RestaurantModel saveRestaurant = restaurantRepository.save(updateRestaurant);
+        return restaurantMapper.toUpdateDTO(saveRestaurant);
         }
+
+        public void deleteRestaurant(UUID id){
+        RestaurantListDTO restaurant = this.findRestaurantById(id);
+        restaurantRepository.deleteById(restaurant.getId());
+        }
+
 
 }
