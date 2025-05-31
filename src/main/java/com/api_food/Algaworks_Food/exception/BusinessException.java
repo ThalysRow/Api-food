d4dbfad5e0b13952b74ae5ce4e9783500d69b727
Thalys Row <thalysrogue@gmail.com>
2025-05-31 19:55:48 -0300

    feat(exception): add BusinessException

---
+++ src/main/java/com/api_food/Algaworks_Food/exception/BusinessException.java
@@ -0,0 +1,13 @@
+package com.api_food.Algaworks_Food.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BusinessException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
