93998538487dad7173be2f2a839807c3daf20ad4
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:15:57 -0300

    refactor(service): adjust all methods in KitchenService

--- src/main/java/com/api_food/Algaworks_Food/service/KitchenService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/KitchenService.java
@@ -1,11 +1,14 @@
 package com.api_food.Algaworks_Food.service;
 
+import com.api_food.Algaworks_Food.dto.create.KitchenCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.KitchenListDTO;
+import com.api_food.Algaworks_Food.dto.update.KitchenUpdateDTO;
 import com.api_food.Algaworks_Food.exception.EntityInUseException;
 import com.api_food.Algaworks_Food.exception.EntityNotFoundException;
 import com.api_food.Algaworks_Food.mapper.KitchenMapper;
-import com.api_food.Algaworks_Food.dto.KitchenDTO;
 import com.api_food.Algaworks_Food.model.KitchenModel;
 import com.api_food.Algaworks_Food.repository.KitchenRepository;
+import com.api_food.Algaworks_Food.utils.StringFormatter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,35 +18,51 @@ import java.util.UUID;
 public class KitchenService {
     private final KitchenMapper kitchenMapper;
     private final KitchenRepository kitchenRepository;
+    private final StringFormatter stringFormatter;
 
-    public KitchenService(KitchenMapper kitchenMapper, KitchenRepository kitchenRepository) {
+    public KitchenService(KitchenMapper kitchenMapper, KitchenRepository kitchenRepository, StringFormatter stringFormatter) {
         this.kitchenMapper = kitchenMapper;
         this.kitchenRepository = kitchenRepository;
+        this.stringFormatter = stringFormatter;
     }
 
-    public KitchenDTO addKitchen(KitchenDTO kitchen){
-        KitchenModel kitchenModel = kitchenMapper.toEntity(kitchen);
-        KitchenModel kitchenSaved = kitchenRepository.save(kitchenModel);
-        return kitchenMapper.toDTO(kitchenSaved);
+    public KitchenCreateDTO addKitchen(KitchenCreateDTO kitchen){
+
+       String formatedName = stringFormatter.stringFormated(kitchen.getName());
+        kitchen.setName(formatedName);
+
+        KitchenModel newKitchen = kitchenMapper.toCreateModel(kitchen);
+        KitchenModel kitchenSaved = kitchenRepository.save(newKitchen);
+        return kitchenMapper.toCreateDTO(kitchenSaved);
     }
 
-    public List<KitchenDTO> listKitchens(){
-        return kitchenRepository.findAll().stream().map(kitchenMapper::toDTO).toList();
+    public List<KitchenListDTO> listKitchens(){
+        return kitchenRepository.findAll().stream().map(kitchenMapper::toCreateListDTO).toList();
     }
 
-    public KitchenDTO findKitchenById(UUID id){
+    public KitchenListDTO findKitchenById(UUID id){
        KitchenModel kitchenFinded = kitchenRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Kitchen not found"));
-       return kitchenMapper.toDTO(kitchenFinded);
+       return kitchenMapper.toCreateListDTO(kitchenFinded);
     }
 
     public void deleteKitchen(UUID id){
-        KitchenDTO kitchenFinded = this.findKitchenById(id);
 
-        if (kitchenFinded.getRestaurants() != null && !kitchenFinded.getRestaurants().isEmpty()){
+        KitchenModel kitchen = kitchenRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("Kitchen not found"));
+
+        if (kitchen.getRestaurants() != null && !kitchen.getRestaurants().isEmpty()){
             throw new EntityInUseException("Kitchen cannot be deleted, it is in use by restaurants.");
         }
 
-        kitchenRepository.deleteById(kitchenFinded.getId());
+        kitchenRepository.deleteById(kitchen.getId());
     }
 
+    public KitchenUpdateDTO updateKitchen(UUID id, KitchenUpdateDTO kitchen){
+        KitchenListDTO kitchenFinded = this.findKitchenById(id);
+        String nameFormated = stringFormatter.stringFormated(kitchen.getName());
+        kitchenFinded.setName(nameFormated);
+
+        KitchenModel newKitchen = kitchenMapper.toModel(kitchenFinded);
+        KitchenModel savedKithen = kitchenRepository.save(newKitchen);
+        return kitchenMapper.toUpdateDTO(savedKithen);
+    }
 }
