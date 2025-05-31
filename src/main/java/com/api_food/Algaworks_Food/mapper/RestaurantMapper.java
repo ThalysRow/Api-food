d4225712c5f26dcaa7eaaa8ee4846c4f5c773248
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:13:20 -0300

    refactor(mapper): add and adjust methods in RestaurantMapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/RestaurantMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/RestaurantMapper.java
@@ -1,11 +1,18 @@
 package com.api_food.Algaworks_Food.mapper;
 
-import com.api_food.Algaworks_Food.dto.RestaurantDTO;
+import com.api_food.Algaworks_Food.dto.create.RestaurantCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.RestaurantListDTO;
+import com.api_food.Algaworks_Food.dto.update.RestaurantUpdateDTO;
 import com.api_food.Algaworks_Food.model.RestaurantModel;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface RestaurantMapper {
-    RestaurantDTO toDTO(RestaurantModel restaurantModel);
-    RestaurantModel toEntity(RestaurantDTO restaurantDTO);
+
+    RestaurantModel toCreateModel(RestaurantCreateDTO restaurantCreateDTO);
+    RestaurantCreateDTO toCreateDTO(RestaurantModel restaurantModel);
+    RestaurantListDTO toListDTO(RestaurantModel restaurantModel);
+    RestaurantModel toUpdateModel(RestaurantListDTO restaurantListDTO);
+    RestaurantUpdateDTO toUpdateDTO(RestaurantModel restaurantModel);
+
 }
