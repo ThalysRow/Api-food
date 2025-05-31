6526121ad3ee1a68e158ef4adc358dbe9e1b2be8
Thalys Row <thalysrogue@gmail.com>
2025-05-31 17:58:33 -0300

    feat(dto): add create restaurant dto

---
+++ src/main/java/com/api_food/Algaworks_Food/dto/create/RestaurantCreateDTO.java
@@ -0,0 +1,31 @@
+package com.api_food.Algaworks_Food.dto.create;
+
+import com.api_food.Algaworks_Food.dto.list.KitchenListDTO;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RestaurantCreateDTO {
+
+    private UUID id;
+
+    @NotBlank(message = "Restaurant name is required")
+    private String name;
+
+    @Positive(message = "The delivery fee must be positive")
+    @NotNull(message = "The delivery fee is required")
+    private BigDecimal deliveryFee;
+
+    @NotNull(message = "Kitchen is required")
+    private KitchenListDTO kitchen;
+
+}
