0adad45dc84d8d42a7bd4275c9fa07e77091fcdb
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:17:05 -0300

    refactor(service): adjust all methods in StateService

--- src/main/java/com/api_food/Algaworks_Food/service/StateService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/StateService.java
@@ -1,9 +1,14 @@
 package com.api_food.Algaworks_Food.service;
 
+import com.api_food.Algaworks_Food.dto.create.StateCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.StateListDTO;
+import com.api_food.Algaworks_Food.dto.update.StateUpdateDTO;
+import com.api_food.Algaworks_Food.exception.EntityInUseException;
+import com.api_food.Algaworks_Food.exception.EntityNotFoundException;
 import com.api_food.Algaworks_Food.mapper.StateMapper;
-import com.api_food.Algaworks_Food.dto.StateDTO;
 import com.api_food.Algaworks_Food.model.StateModel;
 import com.api_food.Algaworks_Food.repository.StateRepository;
+import com.api_food.Algaworks_Food.utils.StringFormatter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,40 +18,48 @@ import java.util.List;
 public class StateService {
     private final StateRepository stateRepository;
     private final StateMapper stateMapper;
+    private final StringFormatter stringFormatter;
 
-    public StateService(StateRepository stateRepository, StateMapper stateMapper) {
+    public StateService(StateRepository stateRepository, StateMapper stateMapper, StringFormatter stringFormatter) {
         this.stateRepository = stateRepository;
         this.stateMapper = stateMapper;
+        this.stringFormatter = stringFormatter;
     }
 
-    public StateDTO createNewState(StateDTO state){
-        StateModel newState = stateMapper.toEntity(state);
+    public StateCreateDTO createNewState(StateCreateDTO state){
+        String nameFormated = stringFormatter.stringFormated(state.getName());
+        state.setName(nameFormated);
+
+        StateModel newState = stateMapper.toCreateModel(state);
         StateModel stateSave = stateRepository.save(newState);
-        return stateMapper.toDTO(stateSave);
+        return stateMapper.toCreateDTO(stateSave);
     }
 
-    public StateDTO findStateById(int id){
-        StateModel findedState = stateRepository.findById(id).orElseThrow(()-> new RuntimeException("State not found"));
-        return stateMapper.toDTO(findedState);
+    public StateListDTO findStateById(int id){
+        StateModel stateFinded = stateRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("State not found"));
+        return stateMapper.toCreateListDTO(stateFinded);
     }
 
     public void delState(int id){
-        StateModel state = stateRepository.findById(id).orElseThrow(()-> new RuntimeException("State not found"));
+        StateModel stateFinded = stateRepository.findById(id).orElseThrow(()-> new EntityNotFoundException("State not found"));
 
-        if(state.getCities() != null && !state.getCities().isEmpty()){
-            throw new IllegalStateException("State in use table cities");
+        if(stateFinded.getCities() != null && !stateFinded.getCities().isEmpty()){
+            throw new EntityInUseException("State cannot be deleted, it is in use by cities.");
         }
-        stateRepository.deleteById(state.getId());
+        stateRepository.deleteById(stateFinded.getId());
     }
 
-    public List<StateDTO> listStates(){
-        return stateRepository.findAll().stream().map(stateMapper::toDTO).toList();
+    public List<StateListDTO> listStates(){
+        return stateRepository.findAll().stream().map(stateMapper::toCreateListDTO).toList();
     }
 
-    public StateDTO updateState(int id, StateDTO state){
-        StateModel findState = stateRepository.findById(id).orElseThrow(()-> new RuntimeException("State not found"));
-        findState.setName(state.getName());
-        StateModel stateSave = stateRepository.save(findState);
-        return stateMapper.toDTO(stateSave);
+    public StateUpdateDTO updateState(int id, StateUpdateDTO state){
+        StateListDTO stateFinded = this.findStateById(id);
+        String nameFormated = stringFormatter.stringFormated(state.getName());
+        stateFinded.setName(nameFormated);
+
+        StateModel newState = stateMapper.toUpdateModel(stateFinded);
+        StateModel saveState = stateRepository.save(newState);
+        return stateMapper.toUpdateDTO(saveState);
     }
 }
