25818bc5210f55a2519f0b3cf4e98619be52eb04
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:01:18 -0300

    feat(dto): add list states dto

---
+++ src/main/java/com/api_food/Algaworks_Food/dto/list/StateListDTO.java
@@ -0,0 +1,13 @@
+package com.api_food.Algaworks_Food.dto.list;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateListDTO {
+    private int id;
+    private String name;
+}
