991a7ec58eae8bf7c99ea3218dbf9f49dfbb7d4b
Thalys Row <thalysrogue@gmail.com>
2025-05-31 18:13:47 -0300

    refactor(mapper): add and adjust methods in StateMapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/StateMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/StateMapper.java
@@ -1,11 +1,16 @@
 package com.api_food.Algaworks_Food.mapper;
 
-import com.api_food.Algaworks_Food.dto.StateDTO;
+import com.api_food.Algaworks_Food.dto.create.StateCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.StateListDTO;
+import com.api_food.Algaworks_Food.dto.update.StateUpdateDTO;
 import com.api_food.Algaworks_Food.model.StateModel;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StateMapper {
-    StateDTO toDTO(StateModel stateModel);
-    StateModel toEntity(StateDTO stateDTO);
+    StateCreateDTO toCreateDTO(StateModel stateModel);
+    StateModel toCreateModel(StateCreateDTO stateCreateDTO);
+    StateListDTO toCreateListDTO(StateModel stateModel);
+    StateModel toUpdateModel(StateListDTO stateListDTO);
+    StateUpdateDTO toUpdateDTO(StateModel stateModel);
 }
