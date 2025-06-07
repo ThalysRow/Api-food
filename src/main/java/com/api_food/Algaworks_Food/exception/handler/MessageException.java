51ab572acf64f6fabbde3fa4521eb452732de35e
Thalys Row <thalysrogue@gmail.com>
2025-06-06 23:38:02 -0300

    feat(exception): add MessageException class for error handling

---
+++ src/main/java/com/api_food/Algaworks_Food/exception/handler/MessageException.java
@@ -0,0 +1,18 @@
+package com.api_food.Algaworks_Food.exception.handler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class MessageException {
+
+    private Integer status;
+    private String type;
+    private String title;
+    private String detail;
+    private LocalDateTime dateTime;
+}
