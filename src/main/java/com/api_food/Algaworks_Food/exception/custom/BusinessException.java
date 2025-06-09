f2953a41128fd107bdbffabebd3f8b7b2104bf6e
Thalys Row <thalysrogue@gmail.com>
2025-06-09 02:53:40 -0300

    refactor(exception): refactored and improved BusinessException

--- src/main/java/com/api_food/Algaworks_Food/exception/custom/BusinessException.java
+++ src/main/java/com/api_food/Algaworks_Food/exception/custom/BusinessException.java
@@ -1,9 +1,19 @@
 package com.api_food.Algaworks_Food.exception.custom;
 
+import java.util.UUID;
+
 public class BusinessException extends RuntimeException{
     private static final long serialVersionUID = 1L;
 
-    public BusinessException(String message, Throwable cause){
-        super(message, cause);
+    public BusinessException(String message){
+        super(message);
+    }
+
+    public BusinessException(String name, int id){
+        this(String.format("The resource '%s' with id '%d' was not found.",name, id));
+    }
+
+    public BusinessException(String name, UUID id){
+        this(String.format("The resource '%s' with id '%s' was not found.", name, id));
     }
 }
