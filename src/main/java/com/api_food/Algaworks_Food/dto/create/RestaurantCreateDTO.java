a23e5c8e20138dd1c3d84256d3821e704a83e6dd
Thalys Row <thalysrogue@gmail.com>
2025-06-09 01:45:07 -0300

    feat(dto): add valid annotaion in restaurantCreateDTO

--- src/main/java/com/api_food/Algaworks_Food/dto/create/RestaurantCreateDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/create/RestaurantCreateDTO.java
@@ -1,9 +1,10 @@
 package com.api_food.Algaworks_Food.dto.create;
 
 import com.api_food.Algaworks_Food.dto.list.KitchenListDTO;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,10 +22,11 @@ public class RestaurantCreateDTO {
     @NotBlank(message = "Restaurant name is required")
     private String name;
 
-    @Positive(message = "The delivery fee must be positive")
+    @PositiveOrZero(message = "The delivery fee must be positive")
     @NotNull(message = "The delivery fee is required")
     private BigDecimal deliveryFee;
 
+    @Valid
     @NotNull(message = "Kitchen is required")
     private KitchenListDTO kitchen;
 
