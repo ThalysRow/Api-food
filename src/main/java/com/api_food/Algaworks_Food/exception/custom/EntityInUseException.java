9b6cb12e00355bb61a51130b2ed1dc2a96a148e5
Thalys Row <thalysrogue@gmail.com>
2025-06-09 02:54:10 -0300

    refactor(exception): refactored and improved EntityInUseException

--- src/main/java/com/api_food/Algaworks_Food/exception/custom/EntityInUseException.java
+++ src/main/java/com/api_food/Algaworks_Food/exception/custom/EntityInUseException.java
@@ -1,9 +1,19 @@
 package com.api_food.Algaworks_Food.exception.custom;
 
+import java.util.UUID;
+
 public class EntityInUseException extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
     public EntityInUseException(String message) {
         super(message);
+    }
+
+    public EntityInUseException(String entityName, UUID entityId, String tableName){
+        this(String.format("'%s' id '%s' cannot be deleted, it is in use by '%s' table.", entityName, entityId, tableName));
+    }
+
+    public EntityInUseException(String entityName, int entityId, String tableName){
+        this(String.format("'%s' id '%s' cannot be deleted, it is in use by '%s' table.", entityName, entityId, tableName));
     }
 }
