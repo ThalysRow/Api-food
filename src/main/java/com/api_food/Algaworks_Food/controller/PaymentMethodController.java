ff8fe6b9adb4aec109f08c76b28db2275f038e60
Thalys Row <thalysrogue@gmail.com>
2025-06-13 02:42:51 -0300

    feat(controller): create list all payment methods route

--- src/main/java/com/api_food/Algaworks_Food/controller/PaymentMethodController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/PaymentMethodController.java
@@ -1,11 +1,14 @@
 package com.api_food.Algaworks_Food.controller;
 
 import com.api_food.Algaworks_Food.dto.create.PaymentMethodCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.PaymentMethodListDTO;
 import com.api_food.Algaworks_Food.service.PaymentMethodService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/paymentMethod")
@@ -21,5 +24,11 @@ public class PaymentMethodController {
     public ResponseEntity<PaymentMethodCreateDTO> createPaymentMethod(@Valid @RequestBody PaymentMethodCreateDTO data){
             PaymentMethodCreateDTO newPayment = paymentMethodService.addPaymentMethod(data);
             return ResponseEntity.status(HttpStatus.CREATED).body(newPayment);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PaymentMethodListDTO>> listAllPaymentMethods(){
+        List<PaymentMethodListDTO> paymentMethods = paymentMethodService.listAllPaymentMethod();
+        return ResponseEntity.status(HttpStatus.OK).body(paymentMethods);
     }
 }
