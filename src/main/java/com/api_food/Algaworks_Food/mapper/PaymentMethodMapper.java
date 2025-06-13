e272d3e3d9e6b8e96a4e6b84f7ebc8d0077f419e
Thalys Row <thalysrogue@gmail.com>
2025-06-13 02:16:59 -0300

    feat(mapper): create PaymentMethodMapper

---
+++ src/main/java/com/api_food/Algaworks_Food/mapper/PaymentMethodMapper.java
@@ -0,0 +1,12 @@
+package com.api_food.Algaworks_Food.mapper;
+
+import com.api_food.Algaworks_Food.dto.create.PaymentMethodCreateDTO;
+import com.api_food.Algaworks_Food.model.PaymentMethodModel;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface PaymentMethodMapper {
+
+    PaymentMethodCreateDTO toCreateDTO(PaymentMethodModel paymentMethodModel);
+    PaymentMethodModel toCreateModel(PaymentMethodCreateDTO paymentMethodCreateDTO);
+}
