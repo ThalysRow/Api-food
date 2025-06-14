73a53b92bcee84e901bc9fbb739e291f0c1cb432
Thalys Row <thalysrogue@gmail.com>
2025-06-14 02:53:12 -0300

    feat(mapper): add toSaveModel and toUpdateDTO payment method mapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/PaymentMethodMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/PaymentMethodMapper.java
@@ -2,6 +2,7 @@ package com.api_food.Algaworks_Food.mapper;
 
 import com.api_food.Algaworks_Food.dto.create.PaymentMethodCreateDTO;
 import com.api_food.Algaworks_Food.dto.list.PaymentMethodListDTO;
+import com.api_food.Algaworks_Food.dto.update.PaymentMethodUpdateDTO;
 import com.api_food.Algaworks_Food.model.PaymentMethodModel;
 import org.mapstruct.Mapper;
 
@@ -11,4 +12,6 @@ public interface PaymentMethodMapper {
     PaymentMethodCreateDTO toCreateDTO(PaymentMethodModel paymentMethodModel);
     PaymentMethodModel toCreateModel(PaymentMethodCreateDTO paymentMethodCreateDTO);
     PaymentMethodListDTO toListDTO(PaymentMethodModel paymentMethodModel);
+    PaymentMethodModel toSaveModel(PaymentMethodListDTO paymentMethodListDTO);
+    PaymentMethodUpdateDTO toUpdateDTO(PaymentMethodModel paymentMethodModel);
 }
