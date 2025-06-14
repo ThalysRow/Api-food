078b54f9ec6b78296bebd43c39418519a37e85e0
Thalys Row <thalysrogue@gmail.com>
2025-06-14 16:47:55 -0300

    feat(controller): add find group by id route

--- src/main/java/com/api_food/Algaworks_Food/controller/GroupController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/GroupController.java
@@ -1,14 +1,12 @@
 package com.api_food.Algaworks_Food.controller;
 
 import com.api_food.Algaworks_Food.dto.create.GroupCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.GroupListDTO;
 import com.api_food.Algaworks_Food.service.GroupService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/groups")
@@ -23,5 +21,11 @@ public class GroupController {
     public ResponseEntity<GroupCreateDTO> newGroup(@Valid @RequestBody GroupCreateDTO data){
         GroupCreateDTO addGroup = groupService.addGroup(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(addGroup);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GroupListDTO> findGroup(@PathVariable int id){
+        GroupListDTO group = groupService.findGroupById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(group);
     }
 }
