cd76b796014f74fe5d7f92ac1c4c01d51e40e550
Thalys Row <thalysrogue@gmail.com>
2025-06-15 03:46:53 -0300

    feat(controller): add find user route

--- src/main/java/com/api_food/Algaworks_Food/controller/UserController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.api_food.Algaworks_Food.controller;
 
 import com.api_food.Algaworks_Food.dto.create.UserCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.UserListDTO;
 import com.api_food.Algaworks_Food.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -24,4 +24,11 @@ public class UserController {
         UserCreateDTO newUser = userService.addNewUser(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserListDTO> findUser(@PathVariable UUID id){
+        UserListDTO user = userService.findUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
 }
