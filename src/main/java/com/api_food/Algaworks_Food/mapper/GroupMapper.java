9a3596f791764bbf6452d5a39c0832ce10597797
Thalys Row <thalysrogue@gmail.com>
2025-06-15 00:22:25 -0300

    feat(mapper): add toUpdateModel and toUpdateDTO in GroupMapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/GroupMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/GroupMapper.java
@@ -2,6 +2,7 @@ package com.api_food.Algaworks_Food.mapper;
 
 import com.api_food.Algaworks_Food.dto.create.GroupCreateDTO;
 import com.api_food.Algaworks_Food.dto.list.GroupListDTO;
+import com.api_food.Algaworks_Food.dto.update.GroupUpdateDTO;
 import com.api_food.Algaworks_Food.model.GroupModel;
 import org.mapstruct.Mapper;
 
@@ -10,4 +11,6 @@ public interface GroupMapper {
     GroupModel toCreateModel(GroupCreateDTO groupCreateDTO);
     GroupCreateDTO toCreateDTO(GroupModel groupModel);
     GroupListDTO toListDTO(GroupModel groupModel);
+    GroupModel toUpdateModel(GroupListDTO groupListDTO);
+    GroupUpdateDTO toUpdateDTO(GroupModel groupModel);
 }
