314e9d6af70ffcfcab4c63a7c3f962b34f692279
Thalys Row <thalysrogue@gmail.com>
2025-06-17 02:24:13 -0300

    feat(mapper): add createUpdateDTO in ProductMapper

--- src/main/java/com/api_food/Algaworks_Food/mapper/ProductMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/ProductMapper.java
@@ -2,6 +2,7 @@ package com.api_food.Algaworks_Food.mapper;
 
 import com.api_food.Algaworks_Food.dto.create.ProductCreateDTO;
 import com.api_food.Algaworks_Food.dto.list.ProductListDTO;
+import com.api_food.Algaworks_Food.dto.update.ProductUpdateDTO;
 import com.api_food.Algaworks_Food.model.ProductModel;
 import org.mapstruct.Mapper;
 
@@ -10,4 +11,5 @@ public interface ProductMapper {
     ProductModel toCreateModel(ProductCreateDTO productCreateDTO);
     ProductCreateDTO toCreateDTO(ProductModel productModel);
     ProductListDTO createListDTO(ProductModel productModel);
+    ProductUpdateDTO createUpdateDTO(ProductModel productModel);
 }
