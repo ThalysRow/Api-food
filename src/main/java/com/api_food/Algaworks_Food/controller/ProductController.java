911ae02874a0bdd726a89ff2efcd6b7c19b6e8d4
Thalys Row <thalysrogue@gmail.com>
2025-06-16 22:58:14 -0300

    feat(controller): add createProduct and listProductsRestaurant routes

---
+++ src/main/java/com/api_food/Algaworks_Food/controller/ProductController.java
@@ -0,0 +1,35 @@
+package com.api_food.Algaworks_Food.controller;
+
+import com.api_food.Algaworks_Food.dto.create.ProductCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.ProductListDTO;
+import com.api_food.Algaworks_Food.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/restaurants/{restaurantId}/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<ProductCreateDTO> createProduct(@PathVariable UUID restaurantId, @RequestBody ProductCreateDTO data) {
+        ProductCreateDTO productCreate = productService.createProduct(restaurantId, data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductListDTO>> listProductsRestaurant(@PathVariable UUID restaurantId) {
+        List<ProductListDTO> products = productService.listProductsRestaurant(restaurantId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(products);
+    }
+
+}
