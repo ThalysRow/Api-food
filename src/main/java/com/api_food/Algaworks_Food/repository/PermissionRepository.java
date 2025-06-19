e88f39e9616e6b69af73b6250ac1c0ea25aa900a
Thalys Row <thalysrogue@gmail.com>
2025-06-19 03:56:23 -0300

    feat(repository): add PermissionRepository

---
+++ src/main/java/com/api_food/Algaworks_Food/repository/PermissionRepository.java
@@ -0,0 +1,7 @@
+package com.api_food.Algaworks_Food.repository;
+
+import com.api_food.Algaworks_Food.model.PermissionModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissionRepository extends JpaRepository<PermissionModel,Integer> {
+}
