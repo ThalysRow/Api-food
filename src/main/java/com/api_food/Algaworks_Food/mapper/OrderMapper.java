1e067dfb7b22ef2463138901ce456e16eafef7fb
Thalys Row <thalysrogue@gmail.com>
2025-06-22 03:33:34 -0300

    feat(mapper): mapped target and source

--- src/main/java/com/api_food/Algaworks_Food/mapper/OrderMapper.java
+++ src/main/java/com/api_food/Algaworks_Food/mapper/OrderMapper.java
@@ -1,7 +1,7 @@
 package com.api_food.Algaworks_Food.mapper;
 
 import com.api_food.Algaworks_Food.dto.create.OrderCreateDTO;
-import com.api_food.Algaworks_Food.dto.create.OrderItemCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.ListProductInOrderDTO;
 import com.api_food.Algaworks_Food.model.OrderItemModel;
 import com.api_food.Algaworks_Food.model.OrderModel;
 import org.mapstruct.Mapper;
@@ -14,8 +14,19 @@ public interface OrderMapper {
     @Mapping(target = "itens", source = "itens")
     OrderModel toCreateModel(OrderCreateDTO orderCreateDTO);
 
-    @Mapping(target = "product.description", ignore = true)
-    @Mapping(target = "product.active", ignore = true)
+    @Mapping(target = "itens", source = "itens")
     OrderCreateDTO toCreateDTO(OrderModel orderModel);
-    List<OrderItemCreateDTO> mapOrderItems(List<OrderItemModel> itens);
+
+    List<ListProductInOrderDTO> mapOrderItems(List<OrderItemModel> itens);
+
+    @Mapping(target = "id", source = "product.id")
+    @Mapping(target = "name", source = "product.name")
+    @Mapping(target = "price", source = "product.price")
+    @Mapping(target = "description", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    @Mapping(target = "quantity", source = "quantity")
+    @Mapping(target = "unitPrice", source = "unitPrice")
+    @Mapping(target = "totalPrice", source = "totalPrice")
+    @Mapping(target = "observations", source = "observations")
+    ListProductInOrderDTO mapOrderItem(OrderItemModel orderItem);
 }
