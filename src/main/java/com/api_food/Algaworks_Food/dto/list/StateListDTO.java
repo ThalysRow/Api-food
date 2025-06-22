ec57f5cd9038c2af357b7fac042350045c7b9256
Thalys Row <thalysrogue@gmail.com>
2025-06-22 02:10:57 -0300

    feat(dto): add read only in name field StateListDTO

--- src/main/java/com/api_food/Algaworks_Food/dto/list/StateListDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/list/StateListDTO.java
@@ -1,5 +1,6 @@
 package com.api_food.Algaworks_Food.dto.list;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -13,5 +14,6 @@ public class StateListDTO {
     @NotNull(message = "State id is required")
     @Positive(message = "State id must be a valid positive number.")
     private int id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String name;
 }
