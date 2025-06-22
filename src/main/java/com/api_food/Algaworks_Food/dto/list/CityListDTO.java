0a45e19056213d25f74484edcdeee5fc45f8844e
Thalys Row <thalysrogue@gmail.com>
2025-06-22 02:04:15 -0300

    feat(dto): add read only in name field cityListDTO

--- src/main/java/com/api_food/Algaworks_Food/dto/list/CityListDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/list/CityListDTO.java
@@ -1,5 +1,6 @@
 package com.api_food.Algaworks_Food.dto.list;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -13,6 +14,7 @@ public class CityListDTO {
     @NotNull(message = "City id is required")
     @Positive(message = "City id must be a valid positive number.")
     private int id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String name;
     private StateListDTO state;
 }
