97332f17d22e9480f316d8e5756d47cb2de9febd
Thalys Row <thalysrogue@gmail.com>
2025-06-22 02:21:09 -0300

    feat(service): add createNewOrder service

---
+++ src/main/java/com/api_food/Algaworks_Food/service/OrderService.java
@@ -0,0 +1,97 @@
+package com.api_food.Algaworks_Food.service;
+
+import com.api_food.Algaworks_Food.dto.create.OrderCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.ProductListDTO;
+import com.api_food.Algaworks_Food.enums.OrderStatus;
+import com.api_food.Algaworks_Food.mapper.OrderMapper;
+import com.api_food.Algaworks_Food.model.*;
+import com.api_food.Algaworks_Food.repository.OrderRepository;
+import com.api_food.Algaworks_Food.utils.StringFormatter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Service
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+    private final StringFormatter stringFormatter;
+    private final RestaurantService restaurantService;
+    private final UserService userService;
+    private final PaymentMethodService paymentMethodService;
+    private final CityService cityService;
+    private final ProductService productService;
+
+    public OrderService(OrderRepository orderRepository, OrderMapper orderMapper, StringFormatter stringFormatter, RestaurantService restaurantService, UserService userService, PaymentMethodService paymentMethodService, CityService cityService, ProductService productService) {
+        this.orderRepository = orderRepository;
+        this.orderMapper = orderMapper;
+        this.stringFormatter = stringFormatter;
+        this.restaurantService = restaurantService;
+        this.userService = userService;
+        this.paymentMethodService = paymentMethodService;
+        this.cityService = cityService;
+        this.productService = productService;
+    }
+
+    @Transactional
+    public OrderCreateDTO createNewOrder(OrderCreateDTO data){
+
+        RestaurantModel restaurant = restaurantService.verifyFieldRestaurant(data.getRestaurant().getId());
+        UserModel user = userService.verifyUserField(data.getUser().getId());
+        PaymentMethodModel paymentMethod = paymentMethodService.verifyPaymentField(data.getPaymentMethod().getId());
+        CityModel city = cityService.verifyCityField(data.getDeliveryAddress().getCity().getId());
+
+        List<Integer> productsId = data.getItens().stream().map(productId -> productId.getProduct().getId()).toList();
+
+        productService.verifyProductsField(productsId);
+
+        BigDecimal deliveryFee = restaurant.getDeliveryFee();
+        BigDecimal subtotal = new BigDecimal(0);
+
+        for (int i = 0; i < data.getItens().size(); i++) {
+            BigDecimal quantity = data.getItens().get(i).getQuantity();
+            int productId = data.getItens().get(i).getProduct().getId();
+
+            ProductListDTO product = productService.findProductInRestaurant(restaurant.getId(), productId);
+
+            subtotal = subtotal.add(product.getPrice().multiply(quantity));
+        }
+
+        BigDecimal totalValue = subtotal.add(deliveryFee);
+
+        OrderModel addOrder =  orderMapper.toCreateModel(data);
+
+        addOrder.setSubtotal(subtotal);
+        addOrder.setDeliveryFee(deliveryFee);
+        addOrder.setTotalValue(totalValue);
+        addOrder.setStatus(OrderStatus.CREATED);
+        addOrder.setDeliveryAddress(stringFormatter.addressFieldsFormatter(addOrder.getDeliveryAddress()));
+        addOrder.getDeliveryAddress().setCity(city);
+        addOrder.setPaymentMethod(paymentMethod);
+        addOrder.setRestaurant(restaurant);
+        addOrder.setUser(user);
+        addOrder.setDateCreated(OffsetDateTime.now());
+        addOrder.setDateConfirmed(OffsetDateTime.now());
+        addOrder.setDateDelivered(OffsetDateTime.now());
+        addOrder.setItens(data.getItens().stream().map(
+                item -> {
+                    ProductModel product = productService.returnProductModel(item.getProduct().getId());
+                    OrderItemModel orderItem = new OrderItemModel();
+                    orderItem.setProduct(product);
+                    orderItem.setQuantity(item.getQuantity());
+                    orderItem.setUnitPrice(product.getPrice());
+                    orderItem.setTotalPrice(product.getPrice().multiply(item.getQuantity()));
+                    orderItem.setObservations(stringFormatter.stringFormated(item.getObservations()));
+                    orderItem.setOrder(addOrder);
+                    return orderItem;
+                }).toList());
+
+        OrderModel savedOrder = orderRepository.save(addOrder);
+
+        return orderMapper.toCreateDTO(savedOrder);
+    }
+
+}
