b409fb900bd102c76a531abb942690235e116343
Thalys Row <thalysrogue@gmail.com>
2025-06-22 18:51:43 -0300

    feat(controller): add findOrderById route

--- src/main/java/com/api_food/Algaworks_Food/controller/OrderController.java
+++ src/main/java/com/api_food/Algaworks_Food/controller/OrderController.java
@@ -1,14 +1,12 @@
 package com.api_food.Algaworks_Food.controller;
 
 import com.api_food.Algaworks_Food.dto.create.OrderCreateDTO;
+import com.api_food.Algaworks_Food.dto.list.OrderListDTO;
 import com.api_food.Algaworks_Food.service.OrderService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
@@ -24,5 +22,11 @@ public class OrderController {
     public ResponseEntity<OrderCreateDTO> createNewOrder(@Valid @RequestBody OrderCreateDTO data) {
         OrderCreateDTO newOrder = orderService.createNewOrder(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(newOrder);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderListDTO> findOrderById(@PathVariable Integer id){
+        OrderListDTO order = orderService.findOrderById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(order);
     }
 }
