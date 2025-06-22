b426c57d55571f86c47c8049313c263dc5e86671
Thalys Row <thalysrogue@gmail.com>
2025-06-22 02:09:16 -0300

    feat(dto): add read only in name field PaymentMethodListDTO

--- src/main/java/com/api_food/Algaworks_Food/dto/list/PaymentMethodListDTO.java
+++ src/main/java/com/api_food/Algaworks_Food/dto/list/PaymentMethodListDTO.java
@@ -1,5 +1,8 @@
 package com.api_food.Algaworks_Food.dto.list;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +11,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentMethodListDTO {
+    @NotNull(message = "The field id is required")
+    @Positive(message = "The field id must be a positive number")
     private int id;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String name;
 }
