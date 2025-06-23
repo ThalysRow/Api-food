eda588470758461b4f1c2c3e1bbbb1dbfd94b876
Thalys Row <thalysrogue@gmail.com>
2025-06-23 06:36:47 -0300

    refactor(service): comment verifyPaymentMethodName and remove from create and update payment service

--- src/main/java/com/api_food/Algaworks_Food/service/PaymentMethodService.java
+++ src/main/java/com/api_food/Algaworks_Food/service/PaymentMethodService.java
@@ -3,19 +3,19 @@ package com.api_food.Algaworks_Food.service;
 import com.api_food.Algaworks_Food.dto.create.PaymentMethodCreateDTO;
 import com.api_food.Algaworks_Food.dto.list.PaymentMethodListDTO;
 import com.api_food.Algaworks_Food.dto.update.PaymentMethodUpdateDTO;
-import com.api_food.Algaworks_Food.exception.custom.BusinessException;
 import com.api_food.Algaworks_Food.exception.custom.EntityInUseException;
-import com.api_food.Algaworks_Food.exception.custom.PaymentMethodAlreadyExistsException;
 import com.api_food.Algaworks_Food.exception.custom.PaymentMethodNotFoundException;
+import com.api_food.Algaworks_Food.exception.custom.PaymentMethodNotFoundInRestaurantException;
 import com.api_food.Algaworks_Food.mapper.PaymentMethodMapper;
 import com.api_food.Algaworks_Food.model.PaymentMethodModel;
+import com.api_food.Algaworks_Food.model.RestaurantModel;
 import com.api_food.Algaworks_Food.repository.PaymentMethodRepository;
 import com.api_food.Algaworks_Food.utils.StringFormatter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PaymentMethodService {
@@ -23,20 +23,24 @@ public class PaymentMethodService {
     private final PaymentMethodRepository paymentMethodRepository;
     private final PaymentMethodMapper paymentMethodMapper;
     private final StringFormatter stringFormatter;
+    private final RestaurantService restaurantService;
 
-    public PaymentMethodService(PaymentMethodRepository paymentMethodRepository, PaymentMethodMapper paymentMethodMapper, StringFormatter stringFormatter) {
+    public PaymentMethodService(PaymentMethodRepository paymentMethodRepository, PaymentMethodMapper paymentMethodMapper, StringFormatter stringFormatter, RestaurantService restaurantService) {
         this.paymentMethodRepository = paymentMethodRepository;
         this.paymentMethodMapper = paymentMethodMapper;
         this.stringFormatter = stringFormatter;
+        this.restaurantService = restaurantService;
     }
 
-    public void verifyPaymentMethodName(String name){
+/*    public void verifyPaymentMethodName(String name){
         Optional<PaymentMethodModel> findName = paymentMethodRepository.findByName(name);
 
         if (findName.isPresent()) {
             throw new PaymentMethodAlreadyExistsException(name);
         }
     }
+
+    */
 
     public void verifyPaymentMethodInUse(int id){
         PaymentMethodModel findPaymentMethod = this.returnPaymentMethodModel(id);
@@ -52,9 +56,6 @@ public class PaymentMethodService {
 
         String nameFormated = stringFormatter.stringFormated(paymentMethod.getName()).toUpperCase();
         paymentMethod.setName(nameFormated);
-
-        this.verifyPaymentMethodName(nameFormated);
-
         PaymentMethodModel newPayment = paymentMethodMapper.toCreateModel(paymentMethod);
         PaymentMethodModel savePaymentMethod = paymentMethodRepository.save(newPayment);
 
@@ -75,7 +76,6 @@ public class PaymentMethodService {
     public PaymentMethodUpdateDTO updatePaymentMethod(int id, PaymentMethodUpdateDTO data){
         PaymentMethodModel payment = this.returnPaymentMethodModel(id);
         String nameFormated = stringFormatter.stringFormated(data.getName()).toUpperCase();
-        this.verifyPaymentMethodName(nameFormated);
         payment.setName(nameFormated);
         PaymentMethodModel saveUpdatePaymentMethod = paymentMethodRepository.save(payment);
         return paymentMethodMapper.toUpdateDTO(saveUpdatePaymentMethod);
@@ -92,8 +92,10 @@ public class PaymentMethodService {
                 .orElseThrow(()-> new PaymentMethodNotFoundException(id));
     }
 
-    public PaymentMethodModel verifyPaymentField(int id){
-        return   paymentMethodRepository.findById(id)
-                .orElseThrow(() -> new BusinessException("payment", id));
+    public PaymentMethodModel verifyPaymentField(UUID restaurantId, int id){
+        RestaurantModel restaurant = restaurantService.returnRestaurantModel(restaurantId);
+        return  restaurant.getPaymentMethods()
+                .stream().filter(payment -> payment.getId() == id)
+                .findFirst().orElseThrow(()-> new PaymentMethodNotFoundInRestaurantException(id, restaurant.getName()));
     }
 }
