2d21036bd3ac2ccdeaac3c817200261628ce1dc1
Thalys Row <thalysrogue@gmail.com>
2025-06-23 06:38:57 -0300

    refactor(repository): refactor orderRepository, optmize find all query

--- src/main/java/com/api_food/Algaworks_Food/repository/OrderRepository.java
+++ src/main/java/com/api_food/Algaworks_Food/repository/OrderRepository.java
@@ -2,6 +2,11 @@ package com.api_food.Algaworks_Food.repository;
 
 import com.api_food.Algaworks_Food.model.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<OrderModel, Integer> {
+    @Query("from OrderModel o join fetch o.user join fetch o.restaurant r join fetch r.kitchen")
+    List<OrderModel> findAll();
 }
