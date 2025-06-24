e406e4aab069301317aebf5f6be3b032b865809f
Thalys Row <thalysrogue@gmail.com>
2025-06-24 06:00:25 -0300

    feat(service): add confirmOrder service

---
+++ src/main/java/com/api_food/Algaworks_Food/service/OrderFlowService.java
@@ -0,0 +1,32 @@
+package com.api_food.Algaworks_Food.service;
+
+import com.api_food.Algaworks_Food.enums.OrderStatus;
+import com.api_food.Algaworks_Food.exception.custom.BusinessException;
+import com.api_food.Algaworks_Food.model.OrderModel;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.OffsetDateTime;
+
+@Service
+public class OrderFlowService {
+
+    private final OrderService orderService;
+
+    public OrderFlowService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @Transactional
+    public void confirmOrder(int orderId){
+        OrderModel order = orderService.returnOrderModel(orderId);
+
+        if (!order.getStatus().equals(OrderStatus.CREATED)){
+            throw new BusinessException(String.format("The order with id '%d' cannot be confirmed, " +
+                    "because the status of the order is '%s'",orderId,order.getStatus()));
+        }
+
+        order.setStatus(OrderStatus.CONFIRMED);
+        order.setDateConfirmed(OffsetDateTime.now());
+    }
+}
