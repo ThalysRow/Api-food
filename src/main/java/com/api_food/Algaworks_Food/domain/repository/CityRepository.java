e57fb2263f2f2eacc1df9a8aae11e568c062f57a
Thalys Row <thalysrogue@gmail.com>
2026-01-19 18:53:02 -0300

    feat(repo): created verify city in restaurants and orders

--- src/main/java/com/api_food/Algaworks_Food/domain/repository/CityRepository.java
+++ src/main/java/com/api_food/Algaworks_Food/domain/repository/CityRepository.java
@@ -1,6 +1,13 @@
 package com.api_food.Algaworks_Food.domain.repository;
 import com.api_food.Algaworks_Food.domain.model.CityModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CityRepository extends JpaRepository<CityModel, Integer> {
+    @Query("SELECT COUNT(r) > 0 FROM RestaurantModel r WHERE r.address.city.id = :cityId")
+    boolean isCityInUseByRestaurant(@Param("cityId") int cityId);
+
+    @Query("SELECT COUNT(o) > 0 FROM OrderModel o WHERE o.deliveryAddress.city.id = :cityId")
+    boolean isCityInUseByOrder(@Param("cityId") int cityId);
 }
