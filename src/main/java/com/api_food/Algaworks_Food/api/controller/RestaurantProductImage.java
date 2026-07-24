94fccde478ff8d56cc5e890a409bdbfa63071033
Thalys Row <thalysrogue@gmail.com>
2026-07-24 04:03:02 -0300

    feat(products): add PUT and DELETE endpoints for product image

--- src/main/java/com/api_food/Algaworks_Food/api/controller/RestaurantProductImage.java
+++ src/main/java/com/api_food/Algaworks_Food/api/controller/RestaurantProductImage.java
@@ -1,23 +1,35 @@
 package com.api_food.Algaworks_Food.api.controller;
 
 import com.api_food.Algaworks_Food.api.dto.input.ImageUpdateInput;
+import com.api_food.Algaworks_Food.api.dto.output.ImageUpdateOutput;
+import com.api_food.Algaworks_Food.domain.service.PhotoProductService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("restaurants/{restaurantId}/products/{productId}/image")
+@RequiredArgsConstructor
 public class RestaurantProductImage {
 
-    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void updateImage(@PathVariable UUID restauranrId,
-                            @PathVariable Long productId,
-                            @Valid ImageUpdateInput image){
+    private final PhotoProductService photoProductService;
 
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ImageUpdateOutput updateImage(@PathVariable UUID restaurantId,
+                                         @PathVariable int productId,
+                                         @Valid ImageUpdateInput image){
+
+        return photoProductService.save(image, restaurantId, productId);
+    }
+
+    @DeleteMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeImage(@PathVariable UUID restaurantId, @PathVariable int productId){
+        photoProductService.removeImage(restaurantId, productId);
     }
 }
